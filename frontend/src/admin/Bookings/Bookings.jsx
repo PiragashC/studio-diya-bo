81dc1b21da4c059919daa91f7586850542bbb6e4
Piragash <pragashconstantine13@gmail.com>
2024-10-11 20:50:48 +0530

    Signed-off-by: Piragash <pragashconstantine13@gmail.com>

--- frontend/src/admin/Bookings/Bookings.jsx
+++ frontend/src/admin/Bookings/Bookings.jsx
@@ -49,7 +49,15 @@ const Bookings = () => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+  
+    const hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+  
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = String(hours % 12 || 12).padStart(2, "0");
+  
+    return `${day}/${month}/${year} ${formattedHours}:${minutes}:${seconds} ${ampm}`;
   };
 
   const exportToPDF = async () => {
@@ -146,6 +154,8 @@ const Bookings = () => {
     };
     doc.text(`Customer: ${customerDetails.name}`, 14, logoHeight + 55); // Change this Y position
     doc.text(`Mobile: ${customerDetails.mobileNumber}`, 14, logoHeight + 65); // Change this Y position
+
+    doc.text(`Order Date/Function Date: ${selectedBooking.orderDate || "-"}`, 14, logoHeight + 75);
 
     // Increase margin to avoid overlap with the table
     const customerDetailsHeight = 100; // Adjust this height to create enough margin after the customer details
